d35026e7fbdc12086236f52a2ad132075d099d9e
Angel Umeh <akcumeh@gmail.com>
2024-10-04 21:53:12 +0100

    BOT WORKS

--- tg-bot-2/index.js
+++ tg-bot-2/index.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const fetch = require("node-fetch");
+import("node-fetch");
 const axios = require("axios");
 const path = require("path");
-const port = process.env.PORT || 3000;
+const port = process.env.port || 5500;
 
 const app = express();
 
-const bot_token = "8176471076:AAE5DBP8c01HzF5xwp7JY-XBC_cY8xRnSwM";
-const tg_api = "https://api.telegram.org/bot" + bot_token;
+const tg_api = "https://api.telegram.org/bot" + process.env.bot_token;
 
 // webhook
 const init = async () => {
-    const res = await fetch("${tg_api}/setWebhook?url=https://tg-bot-2-k0bic325d-angels-projects-9ba5ac71.vercel.app/webhook");
+    const res = await fetch(tg_api + "/setWebhook?url=https://tg-bot-2-ihmn7nq6q-angels-projects-9ba5ac71.vercel.app/webhook");
     const data = await res.json();
     console.log(data);
 }
@@ -50,19 +49,28 @@ app.post("/webhook", async (req, res) => {
     res.sendStatus(200);
 });
 
-app.listen(3000, async () => {
+app.listen(port, async () => {
     console.log("Server is running on port 3000");
     await init();
 });
 
 bot.command("start", ctx=> {
     console.log(ctx.from);
-    bot.telegram.sendMessage(ctx.chat.id, "Hello there! Welcome to Florence.AI! Ask me something...", {})
+    bot.telegram.sendMessage(ctx.chat.id, "Hello there! Welcome to Florence AI! Ask me something...", {})
 });
 
 bot.command("about", ctx=> {
     console.log(ctx.from);
-    bot.telegram.sendMessage(ctx.chat.id, message, {});
+    bot.telegram.sendMessage(ctx.chat.id, "Florence AI is an edtech AI.", {});
 });
 
-bot.launch()
+bot.on(String, ctx=> {
+    console.log(ctx.from);
+    ai_response = "Sorry, this functionality has not been set up yet. Please check later!"; // openai ai_response
+    bot.telegram.sendMessage(ctx.chat.id, ai_response, {});
+});
+
+app.use(bot.webhookCallback("/webhook"));
+bot.telegram.setWebhook("https://tg-bot-2-ndtd8jd5j-angels-projects-9ba5ac71.vercel.app/webhook");
+
+// bot.launch()
