0aaec1c79c8529a2752b1f0d7a805f63bb7be1d4
Angel Umeh <akcumeh@gmail.com>
2024-10-15 03:50:36 +0100

    webhook fix attempt 22

--- Telegram Chatbot/app.py
+++ Telegram Chatbot/app.py
@@ -1,43 +1,48 @@
 from flask import Flask, request
-import requests
-import os
+# import requests
+# import os
 
 app = Flask(__name__)
 
-TOKEN = os.environ.get("bot_token") # set bot_token in vercel env var
-TG_API_URL = f"https://api.telegram.org/bot{TOKEN}"
+# TOKEN = os.environ.get("bot_token") # set bot_token in vercel env var
+# TG_API_URL = f"https://api.telegram.org/bot{TOKEN}"
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.json
+# @app.route('/webhook', methods=['POST'])
+# def webhook():
+#     data = request.json
 
-    if "message" in data:
-        chat_id = data['message']['chat']['id']
-        text = data['message']['text']
+#     if "message" in data:
+#         chat_id = data['message']['chat']['id']
+#         text = data['message']['text']
 
-        if text == '/start':
-            send_message(chat_id, "Welcome to Florence AI.")
-        elif text == '/about':
-            send_message(chat_id, "Florence AI is your homework assistant.")
-        else:
-            send_message(chat_id, ai_response(text))
+#         if text == '/start':
+#             send_message(chat_id, "Welcome to Florence AI.")
+#         elif text == '/about':
+#             send_message(chat_id, "Florence AI is your homework assistant.")
+#         else:
+#             send_message(chat_id, ai_response(text))
     
-    return '', 200
+#     return '', 200
 
 
-def send_message(chat_id, text):
-    url = f"{TG_API_URL}/sendMessage"
-    payload = {
-        'chat_id': chat_id,
-        'text': text,
-    }
-    requests.post(url, json=payload)
+# def send_message(chat_id, text):
+#     url = f"{TG_API_URL}/sendMessage"
+#     payload = {
+#         'chat_id': chat_id,
+#         'text': text,
+#     }
+#     requests.post(url, json=payload)
 
 
-def ai_response(text):
-    ai_response = "Sorry, this service doesn't work yet!"
-    return ai_response
+# def ai_response(text):
+#     ai_response = "Sorry, this service doesn't work yet!"
+#     return ai_response
 
+
+
+@app.route('/')
+def home():
+    return "You are Home."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
