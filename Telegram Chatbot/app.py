05f96d5055bd61d8bdb4a0bb8f11182472dd7d96
Angel Umeh <akcumeh@gmail.com>
2024-10-15 19:32:55 +0100

    attempt 23?

--- Telegram Chatbot/app.py
+++ Telegram Chatbot/app.py
@@ -1,38 +1,41 @@
 from flask import Flask, request
-# import requests
-# import os
+import requests
+import os
 
 app = Flask(__name__)
 
-# TOKEN = os.environ.get("bot_token") # set bot_token in vercel env var
-# TG_API_URL = f"https://api.telegram.org/bot{TOKEN}"
+TOKEN = os.environ.get("bot_token") # set bot_token in vercel env var
+TG_API_URL = f"https://api.telegram.org/bot{TOKEN}"
 
-# @app.route('/webhook', methods=['POST'])
-# def webhook():
-#     data = request.json
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
 
-#     if "message" in data:
-#         chat_id = data['message']['chat']['id']
-#         text = data['message']['text']
+    if "message" in data:
+        chat_id = data['message']['chat']['id']
+        text = data['message']['text']
 
-#         if text == '/start':
-#             send_message(chat_id, "Welcome to Florence AI.")
-#         elif text == '/about':
-#             send_message(chat_id, "Florence AI is your homework assistant.")
-#         else:
-#             send_message(chat_id, ai_response(text))
+        if text == '/start':
+            send_message(chat_id, "Welcome to Florence AI.")
+        elif text == '/about':
+            send_message(chat_id, "Florence AI is your homework assistant.")
+        else:
+            send_message(chat_id, ai_response(text))
     
-#     return '', 200
+    return '', 200
 
 
-# def send_message(chat_id, text):
-#     url = f"{TG_API_URL}/sendMessage"
-#     payload = {
-#         'chat_id': chat_id,
-#         'text': text,
-#     }
-#     requests.post(url, json=payload)
+def send_message(chat_id, text):
+    url = f"{TG_API_URL}/sendMessage"
+    payload = {
+        'chat_id': chat_id,
+        'text': text,
+    }
+    requests.post(url, json=payload)
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 # def ai_response(text):
 #     ai_response = "Sorry, this service doesn't work yet!"
@@ -43,6 +46,3 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "You are Home.", 200
-
-if __name__ == '__main__':
-    app.run()
